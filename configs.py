014d1c5b1ab16cd07af4ebc826b0a5e679838a9d
leffw <93302536+leffw@users.noreply.github.com>
2022-09-03 19:18:34 -0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,3 +1,9 @@
 from os import environ
 
+# Telegram configuration.
 TELEGRAM_API_TOKEN = environ["TELEGRAM_API_TOKEN"]
+
+# Redis configuration.
+REDIS_HOST = environ.get("REDIS_HOST", "127.0.0.1")
+REDIS_PORT = int(environ.get("REDIS_PORT", 6373))
+REDIS_PASS = environ.get("REDIS_PASS")

517508d07096ac8a0574167231e636a11ff6a9eb
leffw <93302536+leffw@users.noreply.github.com>
2022-09-03 17:02:38 -0300

    Add middlewares.py

---
+++ middlewares.py
@@ -0,0 +1,10 @@
+from database import db
+from tinydb import Query
+
+def checkIfExistWallet(func: object):
+    def wrapper(data: object):
+        if db.get(Query().id == data.from_user.id):
+            return func(data)
+        else:
+            return
+    return wrapper

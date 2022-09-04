ca95515b82ba0f3388dc82daf0fc35400bb08743
leffw <93302536+leffw@users.noreply.github.com>
2022-09-04 14:08:51 -0300

    New Feature: Lightning Address

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from chatbot import bot
 from database import db 
 from configs import API_HOST, API_PORT, PUBLIC_URL_ENDPOINT
 
-from fastapi import FastAPI, Body, HTTPException
+from fastapi import FastAPI, Body, HTTPException, Request
 from tinydb import Query
 
 from secrets import token_hex
 from lnbits import Lnbits
-from json import loads
+from json import dumps, loads
 
 import uvicorn
 import telebot
@@ -21,8 +21,33 @@ WEBHOOK_TELEGRAM_TOKEN = token_hex(64)
 api = FastAPI(docs_url=None, redoc_url=None)
 
 @api.post(f"/api/webhook/telegram/{WEBHOOK_TELEGRAM_TOKEN}")
-def telegram_webhook(payload: dict = Body(...)):
+async def telegram_webhook(payload: dict = Body(...)):
     bot.process_new_updates([telebot.types.Update.de_json(payload)])
+
+@api.get("/.well-known/lnurlp/{username}")
+async def lightning_address(username: str, request: Request = Request):
+    callback = f"{request.base_url}lnurl/pay/{username}"
+    minSendable = 1 * 1000
+    maxSendable = 100000000 * 1000
+    return {"callback": callback, "minSendable": minSendable, "maxSendable": maxSendable, "tag": "payRequest"}
+
+@api.get("/lnurl/pay/{username}")
+async def lnurl_pay_create_invoice(username: str, amount: int = 1000):
+    if (amount < 1000):
+        return {"status":"ERROR", "reason": "The minimum amount is 1 sat."}
+
+    wallet = db.get(Query().username == username)
+    if (wallet == None):
+        return {"status":"ERROR", "reason": "Username does not exist."}
+
+    lnbits = Lnbits(wallet["admin_key"], wallet["invoice_key"], url=wallet["api"])
+    invoice = lnbits.create_invoice(amount, webhook=PUBLIC_URL_ENDPOINT)
+    payment_hash = invoice["payment_hash"]
+    payment_request = invoice["payment_request"]
+    
+    redis.set(f"invoice.{payment_hash}", dumps({"id": wallet["id"]}))
+    redis.expire(f"invoice.{payment_hash}", 86400)
+    return {"pr": payment_request}
 
 @api.post("/api/webhook/lnbits")
 def lnbits_webhook(payload: dict = Body(...)):

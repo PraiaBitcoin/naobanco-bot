c8980268f3355ef7d46570688eb698d1ccabe821
leffw <93302536+leffw@users.noreply.github.com>
2022-09-05 18:12:20 -0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,5 +1,8 @@
 from lib.ngrok import get_ngrok_url
+from os.path import exists
 from os import environ
+
+import rsa
 
 # API configuration.
 API_HOST = environ.get("API_HOST", "0.0.0.0")
@@ -37,3 +40,25 @@ BTC_PASS = environ.get("BTC_PASS")
 
 BTC_ZMQ_TX = environ.get("BTC_ZMQ_TX", "tcp://127.0.0.1:28333")
 BTC_NETWORK = environ.get("BTC_NETWORK", "main")
+
+# RSA configuration keys.
+RSA_PUB_KEY = environ.get("RSA_PUB_KEY")
+RSA_PRIVATE_KEY = environ.get("RSA_PRIVATE_KEY")
+if (RSA_PUB_KEY == None) and (RSA_PRIVATE_KEY == None):
+    if exists("./data/rsa_private_key.key") == False:
+        RSA_PUB_KEY, RSA_PRIVATE_KEY = rsa.newkeys(512)
+        with open("./data/rsa_private_key.key", "wb") as w:
+            w.write(RSA_PRIVATE_KEY.save_pkcs1())
+
+        with open("./data/rsa_pub_key.pub", "wb") as w:
+            w.write(RSA_PUB_KEY.save_pkcs1())
+    else:
+        with open("./data/rsa_private_key.key", "rb") as r:
+            RSA_PRIVATE_KEY = r.read()
+
+        with open("./data/rsa_pub_key.pub", "rb") as r:
+            RSA_PUB_KEY = r.read()
+
+if (type(RSA_PRIVATE_KEY) == bytes) and (type(RSA_PUB_KEY) == bytes):
+    RSA_PUB_KEY = rsa.PublicKey.load_pkcs1(RSA_PUB_KEY)
+    RSA_PRIVATE_KEY = rsa.PrivateKey.load_pkcs1(RSA_PRIVATE_KEY)

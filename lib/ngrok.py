8282feeab102de1efc342f52adbe7473bfee5a41
leffw <93302536+leffw@users.noreply.github.com>
2022-09-23 09:43:42 -0300

    Update ngrok.py

--- lib/ngrok.py
+++ lib/ngrok.py
@@ -5,4 +5,4 @@ import requests
 NGROK_WEB_INTERFACE = environ.get("NGROK_WEB_INTERFACE", "http://127.0.0.1:4040")
 
 def get_ngrok_url() -> dict:
-    return requests.get(f"{NGROK_WEB_INTERFACE}/api/tunnels/").json()["tunnels"][0]["public_url"]
+    return requests.get(f"{NGROK_WEB_INTERFACE}/api/tunnels/").json()["tunnels"][0]["public_url"].replace("http://", "https://")

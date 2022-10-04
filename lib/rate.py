2deb4fd414aa7296204b56afb35d87f7ceb1b096
leffw <93302536+leffw@users.noreply.github.com>
2022-10-04 17:00:00 -0300

    Update rate.py

--- lib/rate.py
+++ lib/rate.py
@@ -1,6 +1,6 @@
 from cachetools import cached, TTLCache
 from requests import get
 
-@cached(cache=TTLCache(maxsize=1, ttl=30))
+@cached(cache=TTLCache(maxsize=1, ttl=60))
 def get_price_bitcoin_in_brl() -> dict:
     return get("https://api.biscoint.io/v1/ticker?base=BTC&quote=BRL").json().get("data")
